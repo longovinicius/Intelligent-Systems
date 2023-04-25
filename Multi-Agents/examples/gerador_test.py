1190b1226895c59e88fd05f2e3d2ecac0b4858cd
longovinicius <longo.vinicius@gmail.com>
2023-04-25 17:45:05 -0300

    adding working implementation of gerador and resolvedor

--- Multi-Agents/examples/gerador_test.py
+++ Multi-Agents/examples/gerador_test.py
@@ -5,71 +5,136 @@ from spade.message import Message
 import random
 import time
 
-# ax + y
+
+XMPP_LOGIN = "longovinicius@yax.im"
+XMPP_PASSWORD = "XMPPpassword."
+
+
+def gen_coefs(scale, degree):
+    # coeficientes que garantem raiz na funcao
+    a = random.randint(-scale, scale)
+    b = random.randint(-scale, scale)
+    c = random.randint(-scale, scale)
+    d = random.randint(-scale, scale)
+
+    if degree == 1:  # a != 0
+        while a == 0:
+            a = random.randint(-scale, scale)
+        return a, b, None, None
+
+    if degree == 2:  # delta >= 0
+        delta = b**2 - 4*a*c
+        while delta < 0 or a == 0:
+            a = random.randint(-scale, scale)
+            delta = b**2 - 4*a*c
+        return a, b, c, None
+
+    if degree == 3:  # delta >= 0
+        while True:
+            a = random.randint(-scale, scale)
+            delta = 18*a*b*c*d - 4*b**3*d + b**2*c**2 - 4*a*c**3 - 27*a**2*d**2
+            if delta >= 0 and a != 0:
+                break
+        return a, b, c, d
+
+
 class Gerador(Agent):
-	rand = random.randint(-1000, 1000)
-	a = 0
-	while a == 0:
-		a = random.randint(-100, 100)
-	b = -1 * (a*rand)
+    funcao_grau = 0
+    a, b, c, d = 0, 0, 0, 0
+
+    class funcao_1grau(CyclicBehaviour):
+        async def run(self):
+            print(f"Rodando funcao {Gerador.funcao_grau} grau")
+            res = await self.receive(timeout=5)
+            if res:
+                x = float(res.body)
+                y = float(Gerador.a*x + Gerador.b)
+                print("Enviou para " + str(res.sender) +
+                      " f(", res.body, ") = ", int(y))  # y = ax + b
+                msg = Message(to=str(res.sender))
+                msg.set_metadata("performative", "inform")
+                msg.body = str(int(y))
+                await self.send(msg)
+
+    class funcao_2grau(CyclicBehaviour):
+        async def run(self):
+            print(f"Rodando funcao {Gerador.funcao_grau}grau")
+            res = await self.receive(timeout=5)
+            if res:
+                x = float(res.body)
+                y = float(Gerador.a*x**2 + Gerador.b*x + Gerador.c)
+                print("Enviou para " + str(res.sender) +
+                      " f(", res.body, ") = ", int(y))  # y = ax^2 + bx + c
+                msg = Message(to=str(res.sender))
+                msg.set_metadata("performative", "inform")
+                msg.body = str(int(y))
+                await self.send(msg)
+
+    class funcao_3grau(CyclicBehaviour):
+        async def run(self):
+            print(f"Rodando funcao {Gerador.funcao_grau}grau")
+            res = await self.receive(timeout=5)
+            if res:
+                x = float(res.body)
+                y = float(Gerador.a*x**3 + Gerador.b*x **
+                          2 + Gerador.c*x + Gerador.d)
+                print("Enviou para " + str(res.sender) +
+                      " f(", res.body, ") = ", int(y))  # y = ax^2 + bx^2 + cx + d
+                msg = Message(to=str(res.sender))
+                msg.set_metadata("performative", "inform")
+                msg.body = str(int(y))
+                await self.send(msg)
+
+    class tipo_funcao(CyclicBehaviour):
+        async def run(self):
+            print("Rodando Tipo Funcao")
+            msg = await self.receive(timeout=5)
+            if msg:
+                msg = Message(to=str(msg.sender))
+                msg.set_metadata("performative", "inform")
+                # msg.set_metadata(outro)
+                msg.body = f"{Gerador.funcao_grau} grau"
+                await self.send(msg)
+                print("Respondeu para " + str(msg.sender) + " com " + msg.body)
+
+    async def setup(self):
+
+        degree = random.randint(1, 3)
+        Gerador.funcao_grau = degree
+        Gerador.a, Gerador.b, Gerador.c, Gerador.d = gen_coefs(
+            100, Gerador.funcao_grau)
+        print(f"Grau: {Gerador.funcao_grau}")
+        print(f"Coefs: {Gerador.a, Gerador.b, Gerador.c, Gerador.d}")
+
+        if degree == 1:
+            tf = self.funcao_1grau()
+        elif degree == 2:
+            tf = self.funcao_2grau()
+        else:
+            tf = self.funcao_3grau()
+
+        t1 = Template()
+        t1.set_metadata("performative", "subscribe")
+        # Vincula template performativo/subscribe a funcao tf informada
+        self.add_behaviour(tf, t1)
+
+        ft = self.tipo_funcao()
+        t2 = Template()
+        t2.set_metadata("performative", "request")
+        self.add_behaviour(ft, t2)
 
 
-	class funcao_1grau(CyclicBehaviour):
-		async def run(self):
-			res = await self.receive(timeout=5)
-			if res:
-				x = float(res.body)
-				y = float(Gerador.a*x + Gerador.b)
-				print("Enviou para " + str(res.sender) +
-					  " f(", res.body, ")= ", x, "=>", int(x)) # y = ax + b
-				msg = Message(to=str(res.sender))
-				msg.set_metadata("performative", "inform")
-				msg.body = str(int(y))
-				await self.send(msg)
-
-	class funcao_2grau(CyclicBehaviour):
-		pass    
-	
-	class funcao_3grau(CyclicBehaviour):
-		pass
-
-	class tipo_funcao(CyclicBehaviour):
-		async def run(self):
-			msg = await self.receive(timeout=5)
-			if msg:
-				msg = Message(to=str(msg.sender))
-				msg.set_metadata("performative", "inform")
-				msg.body = f"{self.tipo_funcao}grau"
-				await self.send(msg)
-				print("Respondeu para" + str(msg.sender) + " com " + msg.body)
-
-	async def setup(self):
-		t = Template()
-		t.set_metadata("performative", "subscribe")
-
-		tf = self.funcao_1grau()
-		print("Funcao de 1o grau: ", Gerador.rand)
-		print("Funcao: ", Gerador.a, "x + (", Gerador.b, ")")
-
-		self.add_behaviour(tf, t)
-
-		ft = self.tipo_funcao()
-		template = Template()
-		template.set_metadata("performative", "request")
-		self.add_behaviour(ft, template)
-
-
-gerador = Gerador("longovinicius@jix.im", "Jixpassword.")
-gerador.web.start(hostname="127.0.0.1", port="10000")
-gerador.start()
+gerador = Gerador(XMPP_LOGIN, XMPP_PASSWORD)
+# gerador.web.start(hostname="127.0.0.1", port="10000")
+# gerador.start()
 
 res = gerador.start()
 res.result()
 
 while gerador.is_alive():
-	try:
-		time.sleep(1)
-	except KeyboardInterrupt:
-		gerador.stop()
-		break
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        gerador.stop()
+        break
 print("Agente encerrou!")
---
+++ Multi-Agents/examples/resolvedor_STM.py
@@ -0,0 +1,91 @@
+import time
+import random
+
+from spade.agent import Agent
+from spade.behaviour import FSMBehaviour, State
+from spade.message import Message
+
+STATE_ONE = "GET_TYPE"
+STATE_TWO = "RESOLVER"
+
+XMPP_RECEIVER = "longovinicius@yax.im"
+XMPP_SENDER = "lucastt@yax.im"
+XMPP_PASSWORD = "XMPPpassword."
+
+
+class ExampleFSMBehaviour(FSMBehaviour):
+    async def on_start(self):
+        print(f"FSM starting at initial state {self.current_state}")
+
+    async def on_end(self):
+        print(f"FSM finished at state {self.current_state}")
+        await self.agent.stop()
+
+
+class Get_type(State):
+    async def run(self):
+        print("InformBehav running")
+        # Instantiate the message to Gerador
+        msg = Message(to=XMPP_RECEIVER)
+        # Set the "inform" FIPA performative
+        msg.set_metadata("performative", "request")
+        msg.body = "PASSA O GRAU"                    # Set the message content
+        print(f"msg.body: {msg.body}")
+        await self.send(msg)
+        print("Message sent!")
+
+        res = await self.receive(timeout=10)
+        print(f"Res Get_type_body(): {res.body}")
+        if res:
+            print("Message received!")
+            # msg.body = f"{self.tipo_funcao}grau"
+            # Resolvedor.function_grau = res[-1]
+            print(f"res.body: {res.body}")
+            self.set_next_state(STATE_TWO)
+
+# y = 5X + 1, X1 = 0, y = 1, X2 = 1, y = 6...
+
+
+class Resolver(State):
+    async def run(self):
+        resolvido = False
+        while (not resolvido):
+            print("Encontrou Tipo de Funcao")
+            x = random.randint(-100, 100)
+            # Instantiate the message to Gerador
+            msg = Message(to=XMPP_RECEIVER)
+            msg.set_metadata("performative", "subscribe")
+            msg.body = str(int(x))
+            await self.send(msg)
+
+            res = await self.receive(timeout=5)
+            if res:
+                print(f"Valor de y recebido = {int(res.body)}")
+                if int(res.body) == 0:
+                    print(f"Resolvido com x = {x}!!!!")
+                    resolvido = True
+                else:
+                    print(f"Errado em x = {x} Keep trying...")
+
+
+class FSMAgent(Agent):
+    async def setup(self):
+        fsm = ExampleFSMBehaviour()
+        fsm.add_state(name=STATE_ONE, state=Get_type(), initial=True)
+        fsm.add_state(name=STATE_TWO, state=Resolver())
+        fsm.add_transition(source=STATE_ONE, dest=STATE_TWO)
+        self.add_behaviour(fsm)
+
+
+if __name__ == "__main__":
+    fsmagent = FSMAgent(XMPP_SENDER, XMPP_PASSWORD)
+    future = fsmagent.start()
+    future.result()
+
+    while fsmagent.is_alive():
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            fsmagent.stop()
+            break
+    print("Agent finished")
